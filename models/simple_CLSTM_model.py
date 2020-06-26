6cc938a74eeea003769aa600c29ec1261190ef7b
huxi <chris_huxi@163.com>
2020-06-26 17:53:08 +0200

    start to train normal cnn

---
+++ models/simple_CLSTM_model.py
@@ -0,0 +1,354 @@
+# @Author : Xi Hu
+# @Datetime : 2020/06/20 21:33
+# @File : CNN_LSTM_model.py
+# @Last Modify Time : 2020/06/20 21:33
+# @Contact : chris_huxi@163.com
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import numpy as np
+import torchvision.models as models
+
+import sys
+sys.path.append('../')
+
+import os
+import dataloader.VSLdataset as VSLdataset
+
+import torch.optim as optim
+    
+import matplotlib.pyplot as plt
+
+from torch.utils.tensorboard import SummaryWriter    
+
+
+
+## ------------------------ CRNN module ---------------------- ##
+
+def conv2D_output_size(img_size, padding, kernel_size, stride):
+    # compute output shape of conv2D
+    outshape = (np.floor((img_size[0] + 2 * padding[0] - (kernel_size[0] - 1) - 1) / stride[0] + 1).astype(int),
+                np.floor((img_size[1] + 2 * padding[1] - (kernel_size[1] - 1) - 1) / stride[1] + 1).astype(int))
+    return outshape
+
+
+# 2D CNN encoder train from scratch (no transfer learning)
+class EncoderCNN(nn.Module):
+    def __init__(self, img_x=90, img_y=120, fc_hidden1=512, fc_hidden2=512, drop_p=0.3, CNN_embed_dim=300):
+        super(EncoderCNN, self).__init__()
+
+        self.img_x = img_x
+        self.img_y = img_y
+        self.CNN_embed_dim = CNN_embed_dim
+
+        # CNN architechtures
+        self.ch1, self.ch2, self.ch3, self.ch4 = 32, 64, 128, 256
+        self.k1, self.k2, self.k3, self.k4 = (5, 5), (3, 3), (3, 3), (3, 3)      # 2d kernal size
+        self.s1, self.s2, self.s3, self.s4 = (2, 2), (2, 2), (2, 2), (2, 2)      # 2d strides
+        self.pd1, self.pd2, self.pd3, self.pd4 = (0, 0), (0, 0), (0, 0), (0, 0)  # 2d padding
+
+        # conv2D output shapes
+        self.conv1_outshape = conv2D_output_size((self.img_x, self.img_y), self.pd1, self.k1, self.s1)  # Conv1 output shape
+        self.conv2_outshape = conv2D_output_size(self.conv1_outshape, self.pd2, self.k2, self.s2)
+        self.conv3_outshape = conv2D_output_size(self.conv2_outshape, self.pd3, self.k3, self.s3)
+        self.conv4_outshape = conv2D_output_size(self.conv3_outshape, self.pd4, self.k4, self.s4)
+
+        # fully connected layer hidden nodes
+        self.fc_hidden1, self.fc_hidden2 = fc_hidden1, fc_hidden2
+        self.drop_p = drop_p
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=self.ch1, kernel_size=self.k1, stride=self.s1, padding=self.pd1),
+            nn.BatchNorm2d(self.ch1, momentum=0.01),
+            nn.ReLU(inplace=True),                      
+            # nn.MaxPool2d(kernel_size=2),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=self.ch1, out_channels=self.ch2, kernel_size=self.k2, stride=self.s2, padding=self.pd2),
+            nn.BatchNorm2d(self.ch2, momentum=0.01),
+            nn.ReLU(inplace=True),
+            # nn.MaxPool2d(kernel_size=2),
+        )
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(in_channels=self.ch2, out_channels=self.ch3, kernel_size=self.k3, stride=self.s3, padding=self.pd3),
+            nn.BatchNorm2d(self.ch3, momentum=0.01),
+            nn.ReLU(inplace=True),
+            # nn.MaxPool2d(kernel_size=2),
+        )
+
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(in_channels=self.ch3, out_channels=self.ch4, kernel_size=self.k4, stride=self.s4, padding=self.pd4),
+            nn.BatchNorm2d(self.ch4, momentum=0.01),
+            nn.ReLU(inplace=True),
+            # nn.MaxPool2d(kernel_size=2),
+        )
+
+        self.drop = nn.Dropout2d(self.drop_p)
+        self.pool = nn.MaxPool2d(2)
+        self.fc1 = nn.Linear(self.ch4 * self.conv4_outshape[0] * self.conv4_outshape[1], self.fc_hidden1)   # fully connected layer, output k classes
+        self.fc2 = nn.Linear(self.fc_hidden1, self.fc_hidden2)
+        self.fc3 = nn.Linear(self.fc_hidden2, self.CNN_embed_dim)   # output = CNN embedding latent variables
+
+    def forward(self, x_3d):
+        cnn_embed_seq = []
+        for t in range(x_3d.size(1)):
+            # CNNs
+            x = self.conv1(x_3d[:, t, :, :, :])
+            x = self.conv2(x)
+            x = self.conv3(x)
+            x = self.conv4(x)
+            x = x.view(x.size(0), -1)           # flatten the output of conv
+
+            # FC layers
+            x = F.relu(self.fc1(x))
+            # x = F.dropout(x, p=self.drop_p, training=self.training)
+            x = F.relu(self.fc2(x))
+            x = F.dropout(x, p=self.drop_p, training=self.training)
+            x = self.fc3(x)
+            cnn_embed_seq.append(x)
+
+        # swap time and sample dim such that (sample dim, time dim, CNN latent dim)
+        cnn_embed_seq = torch.stack(cnn_embed_seq, dim=0).transpose_(0, 1)
+        # cnn_embed_seq: shape=(batch, time_step, input_size)
+
+        return cnn_embed_seq
+
+
+
+"""
+    Neural Network: CNN_LSTM
+    Detail: the input crosss cnn model and LSTM model independly, then the result of both concat
+"""
+
+# https://discuss.pytorch.org/t/solved-concatenate-time-distributed-cnn-with-lstm/15435/4
+# https://blog.csdn.net/shanglianlm/article/details/86376627 resnet 用法
+class CLSTM(models.resnet.ResNet):
+    def __init__(self, lstm_hidden_dim, lstm_num_layers, class_num, pretrained=True):
+        #super().__init__(models.resnet.Bottleneck, [3, 4, 6, 3])
+        super().__init__(models.resnet.BasicBlock, [2, 2, 2, 2])
+
+        self.hidden_dim = lstm_hidden_dim
+        self.num_layers = lstm_num_layers
+        self.image_width = 224
+        self.image_height = 224
+        self.class_num = class_num
+        if pretrained:
+            #self.load_state_dict(models.resnet50(pretrained=True).state_dict())
+            self.load_state_dict(models.resnet18(pretrained=False).state_dict())
+
+        _dropout = 0.8
+        #cnn_out_size = 2048
+        cnn_out_size = 512
+        self.lstm = nn.LSTM(cnn_out_size, self.hidden_dim, dropout=_dropout, num_layers=self.num_layers, batch_first=True)
+        
+        # linear
+        self.hidden1_fc = nn.Linear(self.hidden_dim, self.hidden_dim // 2)
+        self.hidden2_fc = nn.Linear(self.hidden_dim // 2, self.class_num)
+        # dropout
+
+        self.dropout = nn.Dropout(p=_dropout)
+
+    # https://github.com/HHTseng/video-classification/blob/master/CRNN/functions.py    
+    def forward(self, x):
+        # size: batch, len, channel, width, height
+        batch_size, timesteps, C, H, W = x.size()
+        c_in = x.view(batch_size * timesteps, C, H, W)
+
+        # ResNet:
+        cnn_x = self.conv1(c_in)
+
+        cnn_x = self.bn1(cnn_x)
+        cnn_x = self.relu(cnn_x)
+        cnn_x = self.maxpool(cnn_x)
+
+        cnn_x = self.layer1(cnn_x)
+
+        cnn_x = self.layer2(cnn_x)
+        cnn_x = self.layer3(cnn_x)
+        cnn_x = self.layer4(cnn_x)
+
+
+        
+        cnn_x = self.avgpool(cnn_x)
+        c_out = torch.flatten(cnn_x, 1) # batch*len, 2048/512
+        lstm_in = c_out.view(batch_size, timesteps, -1)
+        
+        lstm_out, _ = self.lstm(lstm_in)
+        #print(lstm_out.shape)
+        '''
+        print(lstm_out.shape)
+        lstm_out = torch.transpose(lstm_out, 0, 1)
+        print(lstm_out.shape)
+        lstm_out = torch.transpose(lstm_out, 1, 2)
+        print(lstm_out.shape)
+        print(lstm_out.size(2))
+
+        lstm_out = F.max_pool1d(lstm_out, lstm_out.size(2)).squeeze(2)
+        print(lstm_out.shape)
+        '''
+        # linear
+        cnn_lstm_out = self.hidden1_fc(torch.tanh(lstm_out[:,-1,:]))
+        #cnn_lstm_out = self.dropout(cnn_lstm_out) # shall we add the dropout in fc?
+        cnn_lstm_out = self.hidden2_fc(torch.tanh(cnn_lstm_out))
+        # output
+        logit = cnn_lstm_out
+        #print(logit.shape)
+        return logit       
+
+# 测试一下输出的size        
+def test_model():
+    model = CLSTM(lstm_hidden_dim = 10, lstm_num_layers = 2, class_num=8)
+    #batch, len, channel, width, height
+    data = torch.randn(1, 10, 3, 224, 224)
+    output = model(data)
+    print(output,shape)
+
+def load_checkpoint(model, checkpoint_PATH):
+    if checkpoint_PATH != None:
+        model_CKPT = torch.load(checkpoint_PATH)
+        model.load_state_dict(model_CKPT['model_state_dict'])
+        print('loading checkpoint!')
+    return model   
+    
+def train(model_in, num_epochs = 3, load_model = True, freeze_extractor = True):
+    # === dataloader defination ===
+    train_batch_size = 4
+    valid_batch_size = 2
+    test_batch_size = 1
+    dataloaders = VSLdataset.create_dataloader_train_valid_test(train_batch_size, valid_batch_size, test_batch_size)
+    train_dataloader = dataloaders['train']
+    valid_dataloader = dataloaders['valid']
+    test_dataloader = dataloaders['test']
+    # =============================
+    
+    # === every n epochs print ===
+    valid_epoch_step = 1
+    test_epoch_step = 10
+    # ============================
+
+    # === got model ===
+    save_file = os.path.join('../saved_model', 'CLSTM.pth')
+    writer = SummaryWriter('../saved_model/tensorboard_log')
+    if(load_model == True):
+        model = load_checkpoint(model_in, save_file)
+    else:
+        model = model_in
+    # =================
+    print(model)
+    # === freeze some layers against overfitting ===
+    if(freeze_extractor == True):
+        for layer_id, child in enumerate(model.children()):
+            if layer_id < 8: #layer 9 is the fc
+                for param in child.parameters():
+                    param.requires_grad = False
+        optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr = 0.001) #lr=0.0000000001)
+    else:
+        optimizer = optim.Adam(model.parameters(), lr=0.001)
+    # ==============================================
+
+    loss_function = nn.CrossEntropyLoss()
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min',verbose=1,patience=2)
+
+    # === runing no gpu ===
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # device = "cpu"
+    model.to(device)
+    torch.backends.cudnn.benchmark = True
+    # =====================
+    # epoch = 1
+    
+    for epoch in range(num_epochs):
+        # training
+        model.train()
+        train_loss  = 0.0
+        print('Train:')
+
+        for index, (data, target) in enumerate(train_dataloader):
+            data, target = data.to(device), target.to(device)
+            optimizer.zero_grad()
+            output = model(data)
+            loss = loss_function(output, target)
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item()
+            if index % 10  == 9:    # print every 10 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, index + 1, train_loss / 10))
+                writer.add_scalar('Train/Loss', train_loss / 10, epoch * (len(train_dataloader)) + index + 1)
+                writer.flush()
+                train_loss = 0.0
+
+        if (epoch % valid_epoch_step == (valid_epoch_step -1)):
+            # validation
+            class_correct = list(0. for i in range(len(VSLdataset.class_name_to_id_)))
+            class_total = list(0. for i in range(len(VSLdataset.class_name_to_id_)))
+            class_name = list(VSLdataset.class_name_to_id_.keys())
+            model.eval()
+            print('Valid:')
+            loss_eval = 0.0
+            loss_for_display = 0.0
+            for index_eval, (data_eval, target_eval) in enumerate(valid_dataloader):
+                data_eval, target_eval = data_eval.to(device), target_eval.to(device)
+                output_eval = model(data_eval)
+                loss_i = loss_function(output_eval, target_eval).item()
+                loss_for_display += loss_i
+                loss_eval += loss_i
+                
+                _, predicted = torch.max(output_eval, 1)
+
+                c = (predicted == target_eval).squeeze()
+                for i in range(valid_batch_size):
+                    try:
+                        label = target_eval[i]
+                        class_correct[label] += c[i].item()
+                    except:
+                        label = target_eval
+                        class_correct[label] += c.item()
+                    class_total[label] += 1
+                if index_eval % 10  == 9:    # print every 10 mini-batches
+                    print('[%d, %5d] loss: %.3f' %
+                          (epoch + 1, index_eval + 1, loss_for_display / 10))
+                    loss_for_display = 0.0
+
+            for i in range(len(VSLdataset.class_name_to_id_)):
+                accuracy = 100 * (class_correct[i] + 1) / (class_total[i] + 1)
+                print('Accuracy of %5s : %2d %%' % (
+                    class_name[i], accuracy))
+                # Record loss and accuracy from the test run into the writer
+                writer.add_scalar('Valid/Accuracy ' + str(class_name[i]), accuracy, epoch)
+                writer.flush()
+
+            scheduler.step(loss_eval/len(valid_dataloader))
+            writer.add_scalar('Valid/Loss ', loss_eval/len(valid_dataloader), epoch)
+            writer.flush()
+            loss_eval = 0.0
+
+            # 每次 eval 都进行保存
+            # save current model
+            torch.save({
+                'model_state_dict': model.state_dict(),
+                'epoch': epoch,
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': train_loss
+            }, save_file)
+            print("saved model.")
+        # test ==> TODO
+        '''
+        model.eval()
+        if (epoch%test_epoch_step == 0):
+            valid_losses = []
+            print('Test:')
+            for index, batch in enumerate(test_dataloader):
+                
+                print('Epoch: ', epoch, '| Batch_index: ', index, '| data: ',data.shape, '| labels: ', target.shape)
+                #loss = self._val_step(epoch, index, batch)
+                #losses.append(loss)
+        '''
+
+if __name__=='__main__':
+    #test_model()
+    model = CLSTM(lstm_hidden_dim = 32, lstm_num_layers = 2, class_num=8)        
+    train(model, 100, False, False)
