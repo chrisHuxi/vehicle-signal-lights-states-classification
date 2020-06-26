67b2052142dd866254b439e15bc67c821e7bce24
xi hu <chris_huxi@163.com>
2020-06-26 23:21:31 +0200

    naive clstm done, TODO: test

--- models/simple_CLSTM_model.py
+++ models/simple_CLSTM_model.py
@@ -36,18 +36,13 @@ def conv2D_output_size(img_size, padding, kernel_size, stride):
 
 # 2D CNN encoder train from scratch (no transfer learning)
 class EncoderCNN(nn.Module):
-    def __init__(self, img_x=90, img_y=120, fc_hidden1=512, fc_hidden2=512, drop_p=0.3, CNN_embed_dim=300):
+    def __init__(self, img_x=90, img_y=120, fc_hidden1=512, fc_hidden2=512, drop_p=0.6, CNN_embed_dim=512):
         super(EncoderCNN, self).__init__()
 
         self.img_x = img_x
         self.img_y = img_y
         self.CNN_embed_dim = CNN_embed_dim
 
-        # CNN architechtures
-        self.ch1, self.ch2, self.ch3, self.ch4 = 32, 64, 128, 256
-        self.k1, self.k2, self.k3, self.k4 = (5, 5), (3, 3), (3, 3), (3, 3)      # 2d kernal size
-        self.s1, self.s2, self.s3, self.s4 = (2, 2), (2, 2), (2, 2), (2, 2)      # 2d strides
-        self.pd1, self.pd2, self.pd3, self.pd4 = (0, 0), (0, 0), (0, 0), (0, 0)  # 2d padding
 
         # conv2D output shapes
         self.conv1_outshape = conv2D_output_size((self.img_x, self.img_y), self.pd1, self.k1, self.s1)  # Conv1 output shape
@@ -63,27 +58,23 @@ class EncoderCNN(nn.Module):
             nn.Conv2d(in_channels=3, out_channels=self.ch1, kernel_size=self.k1, stride=self.s1, padding=self.pd1),
             nn.BatchNorm2d(self.ch1, momentum=0.01),
             nn.ReLU(inplace=True),                      
-            # nn.MaxPool2d(kernel_size=2),
         )
         self.conv2 = nn.Sequential(
             nn.Conv2d(in_channels=self.ch1, out_channels=self.ch2, kernel_size=self.k2, stride=self.s2, padding=self.pd2),
             nn.BatchNorm2d(self.ch2, momentum=0.01),
             nn.ReLU(inplace=True),
-            # nn.MaxPool2d(kernel_size=2),
         )
 
         self.conv3 = nn.Sequential(
             nn.Conv2d(in_channels=self.ch2, out_channels=self.ch3, kernel_size=self.k3, stride=self.s3, padding=self.pd3),
             nn.BatchNorm2d(self.ch3, momentum=0.01),
             nn.ReLU(inplace=True),
-            # nn.MaxPool2d(kernel_size=2),
         )
 
         self.conv4 = nn.Sequential(
             nn.Conv2d(in_channels=self.ch3, out_channels=self.ch4, kernel_size=self.k4, stride=self.s4, padding=self.pd4),
             nn.BatchNorm2d(self.ch4, momentum=0.01),
             nn.ReLU(inplace=True),
-            # nn.MaxPool2d(kernel_size=2),
         )
 
         self.drop = nn.Dropout2d(self.drop_p)
@@ -122,26 +113,64 @@ class EncoderCNN(nn.Module):
     Neural Network: CNN_LSTM
     Detail: the input crosss cnn model and LSTM model independly, then the result of both concat
 """
+class CLSTM(nn.Module):
+    def __init__(self, lstm_hidden_dim, lstm_num_layers, class_num):
+        super().__init__()
+        
+        self.img_x = 224
+        self.img_y = 224
+        self.CNN_embed_dim = 512
+        #self.fc_hidden1=512
+        #self.fc_hidden2=512
+        
+        # CNN architechtures
+        self.ch1, self.ch2, self.ch3, self.ch4 = 32, 64, 128, 256
+        self.k1, self.k2, self.k3, self.k4 = (5, 5), (3, 3), (3, 3), (3, 3)      # 2d kernal size
+        self.s1, self.s2, self.s3, self.s4 = (2, 2), (2, 2), (2, 2), (2, 2)      # 2d strides
+        self.pd1, self.pd2, self.pd3, self.pd4 = (0, 0), (0, 0), (0, 0), (0, 0)  # 2d padding
+        # conv2D output shapes
+        self.conv1_outshape = conv2D_output_size((self.img_x, self.img_y), self.pd1, self.k1, self.s1)  # Conv1 output shape
+        self.conv2_outshape = conv2D_output_size(self.conv1_outshape, self.pd2, self.k2, self.s2)
+        self.conv3_outshape = conv2D_output_size(self.conv2_outshape, self.pd3, self.k3, self.s3)
+        self.conv4_outshape = conv2D_output_size(self.conv3_outshape, self.pd4, self.k4, self.s4)
+        
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=self.ch1, kernel_size=self.k1, stride=self.s1, padding=self.pd1),
+            nn.BatchNorm2d(self.ch1, momentum=0.01),
+            nn.ReLU(inplace=True),                      
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=self.ch1, out_channels=self.ch2, kernel_size=self.k2, stride=self.s2, padding=self.pd2),
+            nn.BatchNorm2d(self.ch2, momentum=0.01),
+            nn.ReLU(inplace=True),
+        )
 
-# https://discuss.pytorch.org/t/solved-concatenate-time-distributed-cnn-with-lstm/15435/4
-# https://blog.csdn.net/shanglianlm/article/details/86376627 resnet 用法
-class CLSTM(models.resnet.ResNet):
-    def __init__(self, lstm_hidden_dim, lstm_num_layers, class_num, pretrained=True):
-        #super().__init__(models.resnet.Bottleneck, [3, 4, 6, 3])
-        super().__init__(models.resnet.BasicBlock, [2, 2, 2, 2])
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(in_channels=self.ch2, out_channels=self.ch3, kernel_size=self.k3, stride=self.s3, padding=self.pd3),
+            nn.BatchNorm2d(self.ch3, momentum=0.01),
+            nn.ReLU(inplace=True),
+        )
 
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(in_channels=self.ch3, out_channels=self.ch4, kernel_size=self.k4, stride=self.s4, padding=self.pd4),
+            nn.BatchNorm2d(self.ch4, momentum=0.01),
+            nn.ReLU(inplace=True),
+        )
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+        _dropout = 0.8
+        #self.drop = nn.Dropout(_dropout)
+        #self.pool = nn.MaxPool2d(2)
+        #self.fc1 = nn.Linear(self.ch4 * self.conv4_outshape[0] * self.conv4_outshape[1], self.fc_hidden1)   # fully connected layer, output k classes
+        #self.fc2 = nn.Linear(self.fc_hidden1, self.fc_hidden2)
+        #self.fc3 = nn.Linear(self.fc_hidden2, self.CNN_embed_dim)   # output = CNN embedding latent variables
+        
         self.hidden_dim = lstm_hidden_dim
         self.num_layers = lstm_num_layers
-        self.image_width = 224
-        self.image_height = 224
         self.class_num = class_num
-        if pretrained:
-            #self.load_state_dict(models.resnet50(pretrained=True).state_dict())
-            self.load_state_dict(models.resnet18(pretrained=False).state_dict())
 
-        _dropout = 0.8
-        #cnn_out_size = 2048
-        cnn_out_size = 512
+        
+
+        cnn_out_size = 256
         self.lstm = nn.LSTM(cnn_out_size, self.hidden_dim, dropout=_dropout, num_layers=self.num_layers, batch_first=True)
         
         # linear
@@ -149,7 +178,7 @@ class CLSTM(models.resnet.ResNet):
         self.hidden2_fc = nn.Linear(self.hidden_dim // 2, self.class_num)
         # dropout
 
-        self.dropout = nn.Dropout(p=_dropout)
+        #self.dropout = nn.Dropout(p=_dropout)
 
     # https://github.com/HHTseng/video-classification/blob/master/CRNN/functions.py    
     def forward(self, x):
@@ -157,41 +186,31 @@ class CLSTM(models.resnet.ResNet):
         batch_size, timesteps, C, H, W = x.size()
         c_in = x.view(batch_size * timesteps, C, H, W)
 
-        # ResNet:
+        # CNN:
         cnn_x = self.conv1(c_in)
-
-        cnn_x = self.bn1(cnn_x)
-        cnn_x = self.relu(cnn_x)
-        cnn_x = self.maxpool(cnn_x)
-
-        cnn_x = self.layer1(cnn_x)
-
-        cnn_x = self.layer2(cnn_x)
-        cnn_x = self.layer3(cnn_x)
-        cnn_x = self.layer4(cnn_x)
-
-
-        
+        cnn_x = self.conv2(cnn_x)
+        cnn_x = self.conv3(cnn_x)
+        cnn_x = self.conv4(cnn_x)
+        '''
+        print(cnn_x.shape)
+        x = F.relu(self.fc1(x))
+            # x = F.dropout(x, p=self.drop_p, training=self.training)
+        x = F.relu(self.fc2(x))
+        x = F.dropout(x, p=self.drop_p, training=self.training)
+        x = self.fc3(x)
+        '''
+        print(cnn_x.shape)
         cnn_x = self.avgpool(cnn_x)
+        print(cnn_x.shape)
         c_out = torch.flatten(cnn_x, 1) # batch*len, 2048/512
-        lstm_in = c_out.view(batch_size, timesteps, -1)
+      
         
+        lstm_in = c_out.view(batch_size, timesteps, -1)
         lstm_out, _ = self.lstm(lstm_in)
-        #print(lstm_out.shape)
-        '''
-        print(lstm_out.shape)
-        lstm_out = torch.transpose(lstm_out, 0, 1)
-        print(lstm_out.shape)
-        lstm_out = torch.transpose(lstm_out, 1, 2)
-        print(lstm_out.shape)
-        print(lstm_out.size(2))
 
-        lstm_out = F.max_pool1d(lstm_out, lstm_out.size(2)).squeeze(2)
-        print(lstm_out.shape)
-        '''
         # linear
         cnn_lstm_out = self.hidden1_fc(torch.tanh(lstm_out[:,-1,:]))
-        #cnn_lstm_out = self.dropout(cnn_lstm_out) # shall we add the dropout in fc?
+        cnn_lstm_out = self.dropout(cnn_lstm_out) # shall we add the dropout in fc?
         cnn_lstm_out = self.hidden2_fc(torch.tanh(cnn_lstm_out))
         # output
         logit = cnn_lstm_out
