d5b173635422224506e953d2255e0cfcd1b0b566
huxi <chris_huxi@163.com>
2020-08-30 19:58:03 +0200

    res152 for taurus

--- models/CNN_LSTM_model_res152.py
+++ models/CNN_LSTM_model_res152.py
@@ -15,8 +15,8 @@ import sys
 sys.path.append('../')
 
 import os
-import dataloader.VSLdataset as VSLdataset
-#import dataloader.VSLdataset_yoloraw as VSLdataset
+import dataloader.VSLdataset_taurus as VSLdataset
+
 import torch.optim as optim
     
 import matplotlib.pyplot as plt
@@ -30,73 +30,6 @@ import evaluate
     Detail: the input crosss cnn model and LSTM model independly, then the result of both concat
 """
 
-from torch.autograd import Variable
-
-class FocalLoss(nn.Module):
-    r"""
-        This criterion is a implemenation of Focal Loss, which is proposed in 
-        Focal Loss for Dense Object Detection.
-
-            Loss(x, class) = - \alpha (1-softmax(x)[class])^gamma \log(softmax(x)[class])
-
-        The losses are averaged across observations for each minibatch.
-
-        Args:
-            alpha(1D Tensor, Variable) : the scalar factor for this criterion
-            gamma(float, double) : gamma > 0; reduces the relative loss for well-classiﬁed examples (p > .5), 
-                                   putting more focus on hard, misclassiﬁed examples
-            size_average(bool): By default, the losses are averaged over observations for each minibatch.
-                                However, if the field size_average is set to False, the losses are
-                                instead summed for each minibatch.
-
-
-    """
-    def __init__(self, class_num, alpha=None, gamma=2, size_average=True):
-        super(FocalLoss, self).__init__()
-        if alpha is None:
-            self.alpha = Variable(torch.ones(class_num, 1))
-        else:
-            if isinstance(alpha, Variable):
-                self.alpha = alpha
-            else:
-                self.alpha = Variable(alpha)
-        self.gamma = gamma
-        self.class_num = class_num
-        self.size_average = size_average
-
-    def forward(self, inputs, targets):
-        N = inputs.size(0)
-        C = inputs.size(1)
-        P = F.softmax(inputs, dim = 1)
-
-        class_mask = inputs.data.new(N, C).fill_(0)
-        class_mask = Variable(class_mask)
-        ids = targets.view(-1, 1)
-        class_mask.scatter_(1, ids.data, 1.)
-        #print(class_mask)
-
-
-        if inputs.is_cuda and not self.alpha.is_cuda:
-            self.alpha = self.alpha.cuda()
-        alpha = self.alpha[ids.data.view(-1)]
-
-        probs = (P*class_mask).sum(1).view(-1,1)
-
-        log_p = probs.log()
-        #print('probs size= {}'.format(probs.size()))
-        #print(probs)
-
-        batch_loss = -alpha*(torch.pow((1-probs), self.gamma))*log_p 
-        #print('-----bacth_loss------')
-        #print(batch_loss)
-
-
-        if self.size_average:
-            loss = batch_loss.mean()
-        else:
-            loss = batch_loss.sum()
-        return loss
-
 # https://discuss.pytorch.org/t/solved-concatenate-time-distributed-cnn-with-lstm/15435/4
 # https://blog.csdn.net/shanglianlm/article/details/86376627 resnet 用法
 class CLSTM(models.resnet.ResNet):
@@ -104,8 +37,6 @@ class CLSTM(models.resnet.ResNet):
         #super().__init__(models.resnet.Bottleneck, [3, 4, 6, 3]) # 50
         #super().__init__(models.resnet.Bottleneck, [3, 4, 23, 3]) # 101
         super().__init__(models.resnet.Bottleneck, [3, 8, 36, 3]) # 152
-
-        
         #super().__init__(models.resnet.BasicBlock, [2, 2, 2, 2]) # 18
 
         self.hidden_dim = lstm_hidden_dim
@@ -392,7 +323,7 @@ def visualize_mis_class(frames, saved_name, true_label, false_label): # timestep
 if __name__=='__main__':
     #test_model()
     model = CLSTM(lstm_hidden_dim = 512, lstm_num_layers = 3, class_num=8)        
-    train(model_in = model, num_epochs = 100, load_model = False, freeze_extractor = True)
+    train(model_in = model, num_epochs = 100, load_model = False, freeze_extractor = False)
 
     #model = CLSTM(lstm_hidden_dim = 512, lstm_num_layers = 4, class_num=8)      
     #infer(model)
